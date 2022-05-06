3b20a05a1e1c26d25b6ffe6bd22b82869a6391da
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-06 19:39:23 +0300

    Add functionality to the add and remove buttons and display books from redux store

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <form className="newbook">
-    <h2>ADD NEW BOOK</h2>
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Book author" />
-    <button type="submit">Add Book</button>
-  </form>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const actionSubmit = (e) => {
+    e.preventDefault();
+    const newTitle = e.target.title.value;
+    const newAuthor = e.target.author.value;
+
+    const newBook = {
+      id: uuidv4(),
+      title: newTitle,
+      author: newAuthor,
+    };
+
+    if (newTitle.trim() && newAuthor.trim()) {
+      dispatch(addBook(newBook));
+      e.target.title.value = '';
+      e.target.author.value = '';
+    } else {
+      alert('Please write item');
+    }
+  };
+
+  return (
+    <form className="newbook">
+      <h2>ADD NEW BOOK</h2>
+      <input className="title" type="text" placeholder="Book title" />
+      <input className="author" type="text" placeholder="Book author" />
+      <button type="submit" onSubmit={actionSubmit}>Add Book</button>
+    </form>
+  );
+};
 
 export default AddBook;
