33fb220bb37d53ff99cc47ad8a85676539857680
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-06 19:40:55 +0300

    Add functionality to remove button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const { book } = props;
+  const dispatch = useDispatch();
+  const deleteBook = (book) => {
+    dispatch(removeBook(book));
+  };
+
   return (
     <div>
-      <h3>{title}</h3>
-      <p>{author}</p>
-      <button type="button">Remove</button>
+      <h3>{book.title}</h3>
+      <p>{book.author}</p>
+      <button type="button" onClick={() => deleteBook(book.id)}>Remove</button>
     </div>
   );
 };
 
-export default Book;
-
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  book: PropTypes.exact({
+    title: PropTypes.string,
+    author: PropTypes.string,
+    id: PropTypes.string,
+  }).isRequired,
 };
+
+export default Book;
