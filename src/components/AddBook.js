5684de33b9f9edfe605cf97206c4d7756d40dade
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-07 02:48:02 +0300

    Add working functionality of add button

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
   const dispatch = useDispatch();
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState();
 
   const actionSubmit = (e) => {
     e.preventDefault();
-    const newTitle = e.target.title.value;
-    const newAuthor = e.target.author.value;
-
-    const newBook = {
+    dispatch(addBook({
+      title,
+      author,
       id: uuidv4(),
-      title: newTitle,
-      author: newAuthor,
-    };
-
-    if (newTitle.trim() && newAuthor.trim()) {
-      dispatch(addBook(newBook));
-      e.target.title.value = '';
-      e.target.author.value = '';
-    } else {
-      alert('Please write item');
-    }
+    }));
+    setTitle('');
+    setAuthor('');
   };
 
   return (
-    <form className="newbook">
+    <form className="newbook" onSubmit={actionSubmit}>
       <h2>ADD NEW BOOK</h2>
-      <input className="title" type="text" placeholder="Book title" />
-      <input className="author" type="text" placeholder="Book author" />
-      <button type="submit" onSubmit={actionSubmit}>Add Book</button>
+      <input className="title" type="text" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input className="author" type="text" placeholder="Book author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+      <button type="submit">Add Book</button>
     </form>
   );
 };
