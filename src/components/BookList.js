24b265e70b3550ba38148522b6db1877c8ed43c0
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-07 12:24:28 +0300

    Remove hard coded content passed in

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import { getBooks } from '../redux/books/books';
 
 const BookList = () => {
+  const dispatch = useDispatch();
   const bookList = useSelector((state) => state.books);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
 
   return (
     <div>
